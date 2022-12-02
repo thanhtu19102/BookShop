c08dd8b37d54b16b21b1415da15e342b55e5760c
defckvin <87369032+defckvin@users.noreply.github.com>
2022-12-02 18:45:11 +0700

    them model

--- BookShop/Areas/Admin/Models/User.cs
+++ BookShop/Areas/Admin/Models/User.cs
@@ -2,5 +2,59 @@
 {
     public class User
     {
+        private string User_ID;
+        private string FullName;
+        private string UserName;
+        private string Password;
+        private string Email;
+        private string Address;
+        private string Phone;
+        private string Role;
+
+        public string user_ID
+        {
+            get { return User_ID; }
+            set { User_ID = value; }
+        }
+        public string fullname
+        {
+            get { return FullName; }
+            set { FullName = value; }
+        }
+        public string username
+        {
+            get { return UserName; }
+            set { UserName = value; }
+        }
+        public string password
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+
+        public string email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+        public string address
+        {
+            get { return Address; }
+            set { Address = value; }
+        }
+
+        public string phone
+        {
+            get { return Phone; }
+            set { Phone = value; }
+        }
+        public string role
+        {
+            get { return Role; }
+            set { Role = value; }
+        }
+        public User()
+        {
+        }
     }
 }
